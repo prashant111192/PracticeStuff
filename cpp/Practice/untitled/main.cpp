8fa12492e273091941a0d8c02ee71a5627124165
prashant111192 <prashant_1111@hotmail.com>
2021-03-15 12:19:53 +0100

    h7

--- cpp/Practice/untitled/main.cpp
+++ cpp/Practice/untitled/main.cpp
@@ -3,43 +3,93 @@
 using namespace std;
 
 // TypdeDef
-typedef unsigned short int USHORT ;
+typedef unsigned short int USHORT;
 
 //Declarations
-USHORT sum (int, int);
+USHORT sum(int, int);
 void DataSize();
-void Wrap (USHORT);
+void Wrap(USHORT);
 
 //#define
-#define classes  5;
+#define classes 5;
 
 // const
 const int studentsperclass = 6;
 
+//classes
+class Cat
+{
+    public:
+        Cat(int age);
+        ~Cat();
+        void cPrint() const;
+        void cSetAge(int age);
+
+    private:
+        int cAge;
+};
+
+Cat::Cat(int age)
+{
+    cAge = age;
+}
+
+Cat::~Cat()
+{
+}
+
+void Cat::cPrint() const
+{
+    cout << cAge<< endl << "Hey hay"<< endl;
+}
+
+void Cat::cSetAge(int age)
+{
+    cout << "Setting age :";
+    cAge = age;
+}
+
+
 int main()
 {
-    int a=1, b=2; // Defining a,b,c
+    int a = 1, b = 2;                   // Defining a,b,c
     cout << "Hello World! \nLine 2 \n"; //This is the first time I've properly coded in ages so lets see
     //taking the sum
-    USHORT c = sum(a,b);
-    enum Color {red, green=100, blue};
-    int classes_2 = classes
-    int totalStudents;
-    totalStudents = classes_2*studentsperclass;
-    cout << "Print Sum: " << c << endl<< "Classes: " << classes_2 << endl<< "Total Studnets: "<< totalStudents<< endl;
-    cout << Color(red)<<Color(blue)<<endl;
+    USHORT c = sum(a, b);
+    enum Color
+    {
+        red,
+        green = 100,
+        blue
+    };
+    int classes_2 = classes int totalStudents;
+    totalStudents = classes_2 * studentsperclass;
+    cout << "Print Sum: " << c << endl
+         << "Classes: " << classes_2 << endl
+         << "Total Studnets: " << totalStudents << endl;
+    cout << Color(red) << Color(blue) << endl;
 
     //Checking the size of various data types
     //DataSize();
 
     //Wrapping
-    Wrap(c);
+    if (c != 0)
+    {
+        Wrap(c);
+    }
+
+    //Classes
+    cout << "classes\n";
+    Cat apple(1);
+    apple.cPrint();
+    apple.cSetAge(2);
+    apple.cPrint();
 
     return 0;
 }
 
 //Definisitons
-USHORT sum (int a, int b)
+USHORT sum(int a, int b)
 {
     int c;
     c = a + b;
@@ -51,11 +101,11 @@ void DataSize()
 {
 
     cout << "Printing data sizes \n";
-    cout << "int: " << sizeof(int) << ", double: "<< sizeof(double) <<", short: "<< sizeof(short) <<", long: "<< sizeof (long)<< ", char: "<< sizeof (char)<< ", bool: "<< sizeof (bool)<< ", float: " <<sizeof (float) << endl;
+    cout << "int: " << sizeof(int) << ", double: " << sizeof(double) << ", short: " << sizeof(short) << ", long: " << sizeof(long) << ", char: " << sizeof(char) << ", bool: " << sizeof(bool) << ", float: " << sizeof(float) << endl;
 }
 
 //Showcasing wrapping of datatypes
-void Wrap (USHORT c)
+void Wrap(USHORT c)
 {
     c = 65535;
     cout << c << endl;
