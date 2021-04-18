ac77ae9a896c9b6e6efaf70bda78b70a7990051b
prashant111192 <prashant_1111@hotmail.com>
2021-04-18 22:18:33 +0200

    daisy

---
+++ cpp/Practice/Daisy/try.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// function Demonstration Function
+// prints out a useful message
+void DemonstraionFunction()
+{
+    cout << "In Demonstration Function\n";
+}
+
+int main()
+{
+    cout << "in main\n";
+    DemonstraionFunction();
+    cout << "Back in main\n";
+    return 0;  
+}
